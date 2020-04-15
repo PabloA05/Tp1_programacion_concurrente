7d87d6e0a8643975c4f585ad54bcc68745925e26
SantiagoVillane <63175233+SantiagoVillane@users.noreply.github.com>
2020-04-14 23:28:50 -0300

    Add files via upload
    Version 1

---
+++ Trabajo Practico 1/src/Buffer.java
@@ -0,0 +1,32 @@
+
+public class Buffer {
+    private int contenido;
+    private boolean disponible = false;
+
+
+    public synchronized int consumir() {
+        while (!disponible) {
+            try {
+                wait();
+            } catch (InterruptedException excepcion) { }
+
+        }
+        disponible = false;
+        notifyAll();
+        return contenido;
+    }
+    public synchronized void producir(int valor)
+    {
+        while(disponible)
+        {
+            try {
+                wait();
+            }catch (InterruptedException excepcion) { }
+        }
+        contenido= valor;
+        disponible=true;
+        notifyAll();
+    }
+}
+
+
