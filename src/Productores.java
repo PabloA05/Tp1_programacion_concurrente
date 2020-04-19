64461f442087cd8d541aaa79ffa8d497fe77ac83
PabloA05 <pablo.cazon@alumnos.unc.edu.ar>
2020-04-18 23:03:04 -0300

    cocina, add ,discard

--- src/Productores.java
+++ src/Productores.java
@@ -1,20 +1,34 @@
-import java.util.Vector;
-import java.util.Random;
+import java.util.LinkedList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Productores implements Runnable {
     private Buffer buffer;
 
-    private final Vector<Integer> elementos = new Vector();
-    Random rand = new Random();
+
+    private LinkedList<Producto> list_products = new LinkedList<>();
 
     public Productores(Buffer buffer) {
         this.buffer = buffer;
     }
 
-    private synchronized void cocinar() throws Exception {
-
-
+    private void cocinar() {
+        int rand = ThreadLocalRandom.current().nextInt(1, 200 + 1);
+        this.producto_add(new Producto(rand));
+        try {
+            wait(rand); // espera rand entre 1 y 200 ms
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
+    private void producto_add(Producto producto) {
+        list_products.add(producto);
+    }
+
+    private void discard() {
+        list_products.remove();
+    }
+
 
     @Override
     public void run() {
