52370a63c3052f1edbd18f724894bd96a2d29bbe
PabloA05 <pablo.cazon@alumnos.unc.edu.ar>
2020-04-18 21:44:56 -0300

    a

--- src/Productores.java
+++ src/Productores.java
@@ -3,22 +3,18 @@ import java.util.Random;
 
 public class Productores implements Runnable {
     private Buffer buffer;
-    private static final int tam = 25; //capacidad del buffer
-    private final Vector<Integer> elementos = new Vector();
-    Random numeros_aleatorios = new Random();
-    int datos;
+
+    private final Vector<Producto> elementos = new Vector();
+    Random rand = new Random();
 
     public Productores(Buffer buffer) {
         this.buffer = buffer;
     }
 
     private synchronized void cocinar() throws Exception {
-        if (elementos.size() == tam)
-            elementos.clear();//ya que el sistema es pura pérdida al estar lleno el buffer descartamos todo
-        else {
-            datos = numeros_aleatorios.nextInt(10);//genero numeros aleatorios del 0 al 9
-            elementos.add(datos);//agrego los elementos al vector
-        }
+        
+
+
     }
 
     public void run() {
